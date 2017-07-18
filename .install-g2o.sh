04b12718ede380a29f68e93e27840725d56c080d
Charles Wang <w@hxdl.org>
2017-07-18 22:43:34 +0800

    fix

--- .install-g2o.sh
+++ .install-g2o.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [! -d "$G2O_INSTALL_DIR" ]; then
+if [ ! -d "$G2O_INSTALL_DIR" ]; then
     cd g2o/
     mkdir build && cd build
     cmake -DCMAKE_INSTALL_PREFIX="$G2O_INSTALL_DIR" -DCMAKE_BUILD_TYPE=RELEASE ..
